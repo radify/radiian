ba096578bb9e3a09ef2187466f8b2c10e4ea2ed4
Jason Stillerman <jason.t.stillerman@gmail.com>
2016-06-22 15:23:33 -0400

    Regex specs for awsKey and awsSecret

--- spec/lib/regexesSpec.js
+++ spec/lib/regexesSpec.js
@@ -13,10 +13,26 @@ describe('regexes.js', function() {
   });
 
   describe('awsKey', function() {
-    // TODO
+    it('should fail on anything that is not exactly 20 characters long', function(){
+      expect(regexes.awsKey.test("435FFGSGHF5")).toBeFalsy();
+      expect(regexes.awsKey.test("AKLFDSKFDKSFJGSDDSJ467463FDGJGDK7575DFG")).toBeFalsy();
+    });
+    it('should fail on anything that is not all caps', function(){
+      expect(regexes.awsKey.test("ABCDEFGHJLDJVYEHDAFo")).toBeFalsy();
+    });
+    it('should accept on any 20 character alphanumeric uppercase string', function(){
+      expect(regexes.awsKey.test('AKIAIOSFODNN7EXAMPLE')).toBeTruthy();
+    });
   });
 
   describe('awsSecret', function() {
-    // TODO
+    it('should fail on anything that is not exactly 40 characters long', function(){
+      expect(regexes.awsSecret.test("435FFGSGHF5")).toBeFalsy();
+      expect(regexes.awsSecret.test("AKLFDSKFDKSFJGSDDSJ467463FDGJGDK7575D984759485974SDFGSFGSHFGDS45FG")).toBeFalsy();
+    });
+
+    it('should accept on any 40 character alphanumeric string', function(){
+      expect(regexes.awsSecret.test('wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY')).toBeTruthy();
+    });
   });
 });
